5a4f7eb044d4ccc142ca77f9f9a38fae6aa1e1f2
Amit Agarwal <labnol@macbook.local>
2018-07-01 10:30:39 +0530

    Switch to Arrow functions

--- src/server/utils.js
+++ src/server/utils.js
@@ -1,12 +1,14 @@
 const ONE_SECOND = 1000;
+const ONE_MINUTE = ONE_SECOND * 60;
 const START_TIME = Date.now();
+const MAX_RETRIES = 5;
 
-export const expBackoff = func => {
-  for (let n = 0; n < 6; n += 1) {
+const expBackoff = func => {
+  for (let n = 0; n <= MAX_RETRIES; n += 1) {
     try {
       return func();
     } catch (e) {
-      if (n === 5) {
+      if (n === MAX_RETRIES) {
         throw e;
       }
       Utilities.sleep(2 ** n * ONE_SECOND + Math.round(Math.random() * ONE_SECOND));
@@ -15,6 +17,6 @@ export const expBackoff = func => {
   return null;
 };
 
-export function hasCpuTime() {
-  return !(Date.now() - START_TIME > ONE_SECOND * 60 * 4);
-}
+const hasCpuTime = () => !(Date.now() - START_TIME > ONE_MINUTE * 4);
+
+export { expBackoff, hasCpuTime };
