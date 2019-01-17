923199bf6963953fa46992275c1aded11e1a476c
Amit Agarwal <amit@labnol.org>
2019-01-17 11:44:37 +0530

    Switched to Dev Mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
+/**
+ * @file webpack.config.js
+ * @author Amit Agarwal
+ * @email amit@labnol.org
+ *
+ * Google Apps Script Starter Kit
+ * https://github.com/labnol/apps-script-starter
+ */
+
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
