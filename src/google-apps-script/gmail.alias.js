12a4cd6b03fe3c01f2ce6b45bac546f723e392fa
Amit Agarwal <amit@labnol.org>
2019-09-30 13:33:19 +0530

    GAS Samples

---
+++ src/google-apps-script/gmail.alias.js
@@ -0,0 +1,14 @@
+/* Returns the Gmail Aliases of the authenticated user */
+const getGmailAliases = () => {
+  try {
+    const { sendAs = [] } = Gmail.Users.Settings.SendAs.list('me');
+    if (sendAs.length) {
+      return sendAs.map(alias => alias.sendAsEmail);
+    }
+  } catch (f) {
+    console.error(f);
+  }
+  return [Session.getActiveUser().getEmail()];
+};
+
+export default getGmailAliases;
